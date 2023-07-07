3ce198dbd549ce39bddd7e7a6c0bcc8ed136242a
Mohmmedsrour <XxultimatexX2@gmail.com>
2023-07-07 23:38:03 +0300

    new

--- main.js
+++ main.js
@@ -1,38 +1,47 @@
-let section = document.getElementById("Features-section");
-let section1 = document.querySelector(".Features-section .word");
-let card2 = document.querySelectorAll(".card2");
-window.onscroll = function (){
-    if(window.scrollY >= section.offsetTop -500)
-    {
-        section1.style.transform ="translateY(0)";
-        section1.style.opacity ="1";
-    }
-    if(window.scrollY >= section.offsetTop -300)
-    {
-        card2[0].style.setProperty("transform","translateY(0)",);
-        card2[1].style.setProperty("transform","translateY(0)",);
-        card2[2].style.setProperty("transform","translateY(0)",);
-        card2[0].style.setProperty("opacity","1","important");
-        card2[1].style.setProperty("opacity","1","important");
-        card2[2].style.setProperty("opacity","1","important");
-    }
-};
-function* generatorNums (){
-    let index =0;
-    while( true){
-        yield index++
-    }
-};
-let  i = generatorNums();
-
-
-let start = document.querySelector(".start");
+let start = document.querySelector(".w");
 let loader = document.querySelector(".wifi"); 
 window.addEventListener("load",function(e){
     console.log(document.readyState);
     setTimeout(function(){
-        loader.style.cssText =`display:none ;opacity: 0`;
-        document.body.style.cssText=`overflow:initial`;
-        start.style.cssText =`display:initial; opacity: 1;`;
+        start.style.cssText="display:none";
+        document.body.style.overflow="scroll";
+        scrollTo({top:0,
+            left:0,
+            behavior:"instant"});
+    },1000)
+    this.setTimeout(function(){
+    if(this.document.readyState==="complete"){
+        AOS.init();
+    }
     },1000)
 });
+ScrollReveal().reveal('.widget', { interval: 200 });
+
+
+let headerElement = document.querySelectorAll(".header_content li>a");
+let beforeElement =document.querySelector(".mega-menu")
+let lastElemnt = document.getElementsByClassName("2")
+console.log(lastElemnt);
+headerElement[3].addEventListener("click",function(){
+    headerElement[3].classList.toggle("header-js");
+    beforeElement.classList.toggle("mega-menu-js");
+})
+for(i=0;i<=9;i++){
+lastElemnt[i].addEventListener("click",function(){
+    headerElement[3].classList.remove("header-js");
+    beforeElement.classList.remove("mega-menu-js");
+})}
+
+let scrollElemnet = document.querySelector(".scroll-top");
+window.onscroll=function(){
+    if(window.scrollY >= 800)
+    scrollElemnet.style.cssText="visibility: inherit; opacity: 1;";
+    else
+    scrollElemnet.style.cssText="visibility: hidden; opacity: 0;";
+}
+scrollElemnet.addEventListener("click",function(){
+    scrollTo({top:0,
+        left:0,
+        behavior:"smooth"})
+})
+
